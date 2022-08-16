3c945b93262154dcb16a0f4e41e2c4fce79b86db
VladislavaZinoveikina <vladislava.fot@yandex.ru>
2022-08-16 14:30:23 +0500

    db v1.1

---
+++ db/models/drug.js
@@ -0,0 +1,34 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Drug extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      this.belongsToMany(models.User, {
+        through: models.userDrug,
+        foreignKey: 'drug_id',
+        otherKey: 'user_id'
+      })
+    }
+  }
+  Drug.init({
+    name: DataTypes.STRING,
+    price: DataTypes.INTEGER,
+    dicountPrice: DataTypes.INTEGER,
+    discount: DataTypes.BOOLEAN,
+    free: DataTypes.BOOLEAN,
+    image: DataTypes.TEXT,
+    quantity: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Drug',
+  });
+  return Drug;
+};
---
+++ db/models/user.js
@@ -0,0 +1,30 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      this.belongsToMany(models.Drug, {
+        through: models.userDrug,
+        foreignKey: 'user_id',
+        otherKey: 'drug_id'
+      })
+    }
+  }
+  User.init({
+    mail: DataTypes.STRING,
+    login: DataTypes.STRING,
+    password: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'User',
+  });
+  return User;
+};
